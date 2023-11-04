229238c61df9a925e6050474ce51c8ccea50e8e2
wind4869 <windv587@gmail.com>
2023-11-04 09:26:38 +0800

    Add quick sort stack implementation

--- src/main/java/com/wind4869/_01_sort/MergeSort.java
+++ src/main/java/com/wind4869/_01_sort/MergeSort.java
@@ -1,6 +1,6 @@
 package com.wind4869._01_sort;
 
-import java.util.*;
+import java.util.Arrays;
 
 class MergeSort {
     private static int[] temp;
@@ -8,7 +8,8 @@ class MergeSort {
     public static void main(String[] args) {
         int[] nums = new int[]{3, 1, 2, 4, 5};
         temp = new int[nums.length];
-        mergeSort(nums, 0, nums.length - 1);
+//        mergeSort(nums, 0, nums.length - 1);
+        mergeSort(nums);
         Arrays.stream(nums).forEach(System.out::println);
     }
 
@@ -18,7 +19,23 @@ class MergeSort {
         int mid = (left + right) / 2;
         mergeSort(nums, left, mid);
         mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
 
+    private static void mergeSort(int[] nums) {
+        int n = nums.length;
+        for (int size = 1; size < n; size *= 2) {
+            int left = 0;
+            while (left < n) {
+                int right = Math.min(n - 1, left + 2 * size - 1);
+                int mid = (left + right) / 2;
+                merge(nums, left, mid, right);
+                left = right + 1;
+            }
+        }
+    }
+
+    private static void merge(int[] nums, int left, int mid, int right) {
         int i = left, j = mid + 1, k = left;
         while (i <= mid && j <= right) {
             if (nums[i] <= nums[j]) temp[k++] = nums[i++];
@@ -28,8 +45,8 @@ class MergeSort {
         while (i <= mid) temp[k++] = nums[i++];
         while (j <= right) temp[k++] = nums[j++];
 
-        for (k = left; k <= right; k++) {
-            nums[k] = temp[k];
+        for (i = left; i < k; i++) {
+            nums[i] = temp[i];
         }
     }
 }
--- src/main/java/com/wind4869/_01_sort/QuickSort.java
+++ src/main/java/com/wind4869/_01_sort/QuickSort.java
@@ -1,24 +1,45 @@
 package com.wind4869._01_sort;
 
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
 
 class QuickSort {
     public static void main(String[] args) {
         int[] nums = new int[]{3, 1, 2, 4, 5};
-        quickSort(nums, 0, nums.length - 1);
+//        quickSort(nums, 0, nums.length - 1);
+        quickSort(nums);
         Arrays.stream(nums).forEach(System.out::println);
     }
 
     private static void quickSort(int[] nums, int left, int right) {
         if (left >= right) return;
+        int j = partition(nums, left, right);
+        quickSort(nums, left, j);
+        quickSort(nums, j + 1, right);
+    }
+
+    private static void quickSort(int[] nums) {
+        Deque<int[]> stack = new ArrayDeque<>();
+        stack.push(new int[]{0, nums.length - 1});
+        while (!stack.isEmpty()) {
+            int[] range = stack.pop();
+            int left = range[0], right = range[1];
+            if (left >= right) continue;
+            int j = partition(nums, left, right);
+            stack.push(new int[]{j + 1, right});
+            stack.push(new int[]{left, j});
+        }
+    }
+
+    private static int partition(int[] nums, int left, int right) {
         int i = left - 1, j = right + 1, pivot = nums[(left + right) / 2];
         while (i < j) {
             do i++; while (nums[i] < pivot);
             do j--; while (nums[j] > pivot);
             if (i < j) swap(nums, i, j);
         }
-        quickSort(nums, left, j);
-        quickSort(nums, j + 1, right);
+        return j;
     }
 
     private static void swap(int[] nums, int i, int j) {
